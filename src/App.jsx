ae4d924f8d3001d751a554db2c5e9431c9e7afeb
Vittoria98 <vittoriasacchi7@gmail.com>
2025-04-11 16:39:41 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+import "./App.css";
+import Weather from "./Component/Weather";
+import Header from "./Component/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/MiniCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MiniCard = () => {
+  return <div>MINI CARD</div>;
+};
+
+export default MiniCard;
