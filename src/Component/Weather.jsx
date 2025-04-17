a325ff65aa27e217762341e1eba57cd0b75c1b45
Vittoria Sacchi <vittoriasacchi7@gmail.com>
2025-04-17 17:35:48 +0200

    Update Weather.jsx

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -1,71 +1,97 @@
-import React, { useState } from "react";
-import "./Header.css";
+import React, { useEffect, useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
-const Header = () => {
-  const [data, setData] = useState(null);
-  const [location, setLocation] = useState("");
-  const API_KEY = "c399fba5369c3ecf1bb3158e1137b374";
+const Tempapp = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Torino");
+  const apiKey = "c399fba5369c3ecf1bb3158e1137b374";
 
-  const searchLocation = (event) => {
-    if (event.key === "Enter") {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
-      )
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setData(data);
-        })
-        .catch((error) => {
-          console.error("Error fetching weather data:", error);
-        });
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=metric`
+        );
+        if (res.ok) {
+          const resJson = await res.json();
+          setCity(resJson);
+        } else {
+          console.error("Error fetching data:", res.statusText);
+        }
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+    if (search) {
+      fetchApi();
     }
-  };
+  }, [search, apiKey]);
 
   return (
-    <section className="header-section">
-      <div className="container d-flex justify-content-center align-items-center vh-100">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="me-5">
-              <i class="bi bi-geo-alt"></i>
-              <span>London, UK</span>
+    <>
+      <Container fluid={true}>
+        <Row className="align-items-center">
+          <Col xs={3} md={6} lg={12}>
+            <section className="header-section d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-center">
+                <i class="bi bi-geo-alt"></i>
+                <span className="ms-2">London, UK</span>
+              </div>
+              <div className="d-flex align-items-center">
+                <input
+                  type="text"
+                  className="search text-center text-black rounded-2"
+                  placeholder="Search for a city"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="d-flex align-items-center">
+                <i class="bi bi-calendar-date"></i>
+                <i class="bi bi-bell ms-2"></i>
+              </div>
+            </section>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid={true}>
+        <Row className="justify-content-center">
+          <Col xs={12} md={10} lg={8}>
+            <div className="text-center mt-5">
+              {city ? (
+                <div className="info ">
+                  <h2 className="location mb-4 mt-1">
+                    <i class="fas fa-street-view mt-5"></i> {search}
+                  </h2>
+                  <h1 className="temp mb-4">{city.main.temp}°C</h1>
+                  <h3 className="tempmin_max mb-4">
+                    Min: {city.main.temp_min}°C / Max: {city.main.temp_max}°C
+                  </h3>
+                  <p className="humidity mb-4 fs-4">
+                    Humidity: {city.main.humidity}%
+                  </p>
+                  <p className="wind-speed fs-4">
+                    Wind Speed: {city.wind.speed} m/s
+                  </p>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+                    alt="Weather Icon"
+                    className="weather-icon  "
+                  />
+                </div>
+              ) : (
+                <p>Search for a city</p>
+              )}
             </div>
-            <div className="ms-5">
-              <i class="bi bi-search"></i>
-              <input
-                className="text-light"
-                type="text"
-                placeholder="Search here"
-                value={location}
-                onChange={(event) => setLocation(event.target.value)}
-                onKeyDown={searchLocation}
-              />
-            </div>
-            <div className="ms-5">
-              <i class="bi bi-calendar-date ms-5 text-black"></i>
-              <i class="bi bi-bell"></i>
-            </div>
-          </div>
-          {data && (
-            <div className="col-md-6 weather-card">
-              <h2>Weather in {data.name}</h2>
-              <p>Temperature: {data.main.temp}°C</p>
-              <p>Humidity: {data.main.humidity}%</p>
-              <p>Wind Speed: {data.wind.speed} m/s</p>
-              <p>Description: {data.weather[0].description}</p>
-            </div>
-          )}
-        </div>
-      </div>
-    </section>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
-export default Header;
+export default Tempapp;
