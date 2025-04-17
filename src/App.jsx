4da1aa81183b8773763d81af9d22ebf10bc64aae
Vittoria Sacchi <vittoriasacchi7@gmail.com>
2025-04-17 17:37:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import react from "react";
-
 import "./App.css";
-import Weather from "./Component/Weather";
-import Header from "./Component/Header";
+
+import Tempapp from "./Component/Wheather";
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <Tempapp />
     </div>
   );
 }
